67ab69d1d7782b32a5a74dc84f185a3ef09b723a
Mitinul <torabi9030@gmail.com>
2024-04-04 15:23:56 +0330

    Adding level2

---
+++ levels/Level2 copy.lua
@@ -0,0 +1,67 @@
+-- Level2 = Class { __includes = BaseState }
+
+-- function Level2:init()
+--     self.world = love.physics.newWorld(0, GRAVITY)
+
+--     self.ground = Ground(self.world, GROUND_HEIGHT)
+
+--     -- walls
+--     local leftWallBody = love.physics.newBody(self.world, 0, 0, 'static')
+--     local rightWallBody = love.physics.newBody(self.world, VIRTUAL_WIDTH, 0, 'static')
+--     local wallShape = love.physics.newEdgeShape(0, 0, 0, VIRTUAL_HEIGHT)
+--     love.physics.newFixture(leftWallBody, wallShape)
+--     love.physics.newFixture(rightWallBody, wallShape)
+
+--     self.ball = Ball(self.world, VIRTUAL_WIDTH / 2, VIRTUAL_HEIGHT / 2, BALL_RADIUS, BALL_BOUNCE, BALL_DAMPING)
+
+--     self.player1 = Player(self.world,
+--         VIRTUAL_WIDTH - 4 * PLAYER_RADIUS,
+--         VIRTUAL_HEIGHT - GROUND_HEIGHT - PLAYER_RADIUS,
+--         PLAYER_RADIUS, PLAYER_MASS, PLAYER_SPEED, PLAYER_JUMP, PLAYER_GSCALE,
+--         'left', 'right', 'up')
+
+--     self.player2 = Player(self.world,
+--         4 * PLAYER_RADIUS,
+--         VIRTUAL_HEIGHT - GROUND_HEIGHT - PLAYER_RADIUS,
+--         PLAYER_RADIUS, PLAYER_MASS, PLAYER_SPEED, PLAYER_JUMP, PLAYER_GSCALE,
+--         'a', 'd', 'w')
+
+--     self.basket = Basket(self.world, 600, 1500, 300, 150)
+
+--     self.ballInBasketTimer = 0
+-- end
+
+-- function Level2:update(dt)
+--     self.world:update(dt)
+--     self.player1:update(dt)
+--     self.player2:update(dt)
+
+--     if self.basket:ballIsInside(self.ball) then
+--         self.ballInBasketTimer = self.ballInBasketTimer + dt
+--         if self.ballInBasketTimer > 3 then
+--             GameState:change('start')
+--         end
+--     else
+--         self.ballInBasketTimer = 0
+--     end
+
+--     if love.keyboard.wasPressed('e') then
+--         self.ball.body:destroy()
+--         self.ball = Ball(self.world, VIRTUAL_WIDTH / 2, VIRTUAL_HEIGHT / 2, BALL_RADIUS, BALL_BOUNCE, BALL_DAMPING)
+--     end
+-- end
+
+-- function Level2:render()
+--     self.ground:render()
+--     self.ball:render()
+--     self.player1:render()
+--     self.player2:render()
+--     self.basket:render()
+--     love.graphics.setColor(.5, 1, 0)
+--     if self.ballInBasketTimer > 0 then
+--         love.graphics.printf(math.ceil(3 - self.ballInBasketTimer),
+--             self.basket.body:getX() - self.basket.width / 2, self.basket.body:getY() - 6,
+--             self.basket.width, 'center')
+--     end
+--     love.graphics.setColor(WHITE)
+-- end
--- levels/Level2.lua
+++ levels/Level2.lua
@@ -12,21 +12,16 @@ function Level2:init()
     love.physics.newFixture(leftWallBody, wallShape)
     love.physics.newFixture(rightWallBody, wallShape)
 
-    self.ball = Ball(self.world, VIRTUAL_WIDTH / 2, VIRTUAL_HEIGHT / 2, BALL_RADIUS, BALL_BOUNCE, BALL_DAMPING)
+    self.ball = Ball(self.world, 1200, VIRTUAL_HEIGHT / 2, BALL_RADIUS, BALL_BOUNCE, BALL_DAMPING)
 
     self.player1 = Player(self.world,
-        VIRTUAL_WIDTH - 4 * PLAYER_RADIUS,
+        VIRTUAL_WIDTH / 2,
         VIRTUAL_HEIGHT - GROUND_HEIGHT - PLAYER_RADIUS,
         PLAYER_RADIUS, PLAYER_MASS, PLAYER_SPEED, PLAYER_JUMP, PLAYER_GSCALE,
         'left', 'right', 'up')
 
-    self.player2 = Player(self.world,
-        4 * PLAYER_RADIUS,
-        VIRTUAL_HEIGHT - GROUND_HEIGHT - PLAYER_RADIUS,
-        PLAYER_RADIUS, PLAYER_MASS, PLAYER_SPEED, PLAYER_JUMP, PLAYER_GSCALE,
-        'a', 'd', 'w')
-
-    self.basket = Basket(self.world, 600, 1500, 300, 150)
+    self.basket = Basket(self.world, VIRTUAL_WIDTH - 1200, 1550, 300, 300)
+    self.basket.body:setAngle(math.rad(90))
 
     self.ballInBasketTimer = 0
 end
@@ -34,20 +29,15 @@ end
 function Level2:update(dt)
     self.world:update(dt)
     self.player1:update(dt)
-    self.player2:update(dt)
 
     if self.basket:ballIsInside(self.ball) then
         self.ballInBasketTimer = self.ballInBasketTimer + dt
         if self.ballInBasketTimer > 3 then
-            GameState:change('start')
+            table.insert(PASSED_LEVELS, 2)
+            GameState:change('start', 3)
         end
     else
         self.ballInBasketTimer = 0
-    end
-
-    if love.keyboard.wasPressed('e') then
-        self.ball.body:destroy()
-        self.ball = Ball(self.world, VIRTUAL_WIDTH / 2, VIRTUAL_HEIGHT / 2, BALL_RADIUS, BALL_BOUNCE, BALL_DAMPING)
     end
 end
 
@@ -55,12 +45,12 @@ function Level2:render()
     self.ground:render()
     self.ball:render()
     self.player1:render()
-    self.player2:render()
     self.basket:render()
-    love.graphics.setColor(.5, 1, 0)
+    love.graphics.setFont(FontPrimaryMedium)
+    love.graphics.setColor(BASKET_COLOR)
     if self.ballInBasketTimer > 0 then
         love.graphics.printf(math.ceil(3 - self.ballInBasketTimer),
-            self.basket.body:getX() - self.basket.width / 2, self.basket.body:getY() - 6,
+            self.basket.body:getX() - self.basket.width / 2, self.basket.body:getY() - 50,
             self.basket.width, 'center')
     end
     love.graphics.setColor(WHITE)
